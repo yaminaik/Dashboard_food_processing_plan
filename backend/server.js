f6cdd2ecb291b397dbe75195ce032f84e43a0eb7
Yami Naik <yaminaik@yamis-air.mynetworksettings.com>
2024-04-29 13:18:01 -0400

    Dashboard Backend and Frontend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,33 @@
 const express = require('express');
-const cors = require('cors');
-
+const path = require('path');
+const multer = require('multer');
+const XLSX = require('xlsx');
 const app = express();
+const cors = require('cors');
 app.use(cors());
-app.use(express.json());
+const storage = multer.memoryStorage(); // Using memory storage to store file temporarily
+const upload = multer({ storage: storage });
 
-const port = 5001;
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+
+    // Read the uploaded file from memory
+    const workbook = XLSX.read(req.file.buffer, { type: 'buffer' });
+
+    // Get the name of the first worksheet
+    const firstSheetName = workbook.SheetNames[0];
+
+    // Get the worksheet
+    const worksheet = workbook.Sheets[firstSheetName];
+
+    // Convert worksheet to JSON
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    // Send the JSON data back to the client
+    res.json(data);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
